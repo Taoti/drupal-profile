2428d4d5a04e96d5720b8981a9de9f80e57547a8
Rafael "Zoio" Silva <zoiosilva@users.noreply.github.com>
2023-11-23 13:36:00 -0400

    Update settings.taoti.php
    Add a URL method whitelist.

--- assets/settings.taoti.php
+++ assets/settings.taoti.php
@@ -1,6 +1,14 @@
 <?php
+
+// phpcs:ignoreFile
+
 /**
+ * @file
  * Taoti specific settings.
+ *
+ * IMPORTANT NOTE:
+ * Do not modify this file. This file is always overwritten by the composer
+ * install or update commands.
  */
 
 /*
@@ -30,78 +38,95 @@ if ((isset($_ENV['PANTHEON_ENVIRONMENT']) && $_ENV['PANTHEON_ENVIRONMENT'] !== '
  */
 $settings['config_sync_directory'] = dirname(DRUPAL_ROOT) . '/config/sync';
 
+if (!isset($_SERVER['REQUEST_URI'])) {
+  return;
+}
+
+/**
+ * Whitelist urls methods that could contains strings that might be wrongly
+ * caught by the banned urls list.
+ */
+$whitelisted_methods = [
+  '/openid-connect',
+];
+
+foreach($whitelisted_methods as $method) {
+  if (str_starts_with($_SERVER['REQUEST_URI'], $method)) {
+    return;
+  }
+}
+
 /**
  * Hard block urls that shouldn't cause system load.
  * Don't bother even redirecting or giving a friendly error page. There is no
  * legitimate reason to be accessing these urls on a Pantheon/Amazee Drupal
  * install.
  */
-if (isset($_SERVER['REQUEST_URI'])) {
-  $banned_urls = [
-    // General.
-    'admin.php',
-    'api.zip',
-    'autodiscover.xml',
-    '.aws',
-    'backup.tar',
-    'back.zip',
-    'back.tar',
-    'backup.zip',
-    'backup.tar',
-    'beez',
-    'bkp',
-    'build.xml',
-    'CMSHelp',
-    'CMSPages',
-    'CMSTemplates',
-    'db.php',
-    'dbconfig.zip',
-    'DesktopModule',
-    '_dump',
-    '.env',
-    'installer.php',
-    'item.php',
-    'mailer.php',
-    'magento',
-    'old/',
-    'preview.php',
-    '_profiler',
-    'sitecore',
-    'sitecorex',
-    'sitedata',
-    'site-tools',
-    'Site-Tools',
-    '.sql',
-    '.well-known',
-    'xleet',
-    'xmlrpc.php',
-    'var/',
-    'vendor/',
-    'wwwback',
-    'wwwdata',
-    'wwwroot',
+$banned_urls = [
+  // General.
+  'admin.php',
+  'api.zip',
+  'autodiscover.xml',
+  '.aws',
+  'backup.tar',
+  'back.zip',
+  'back.tar',
+  'backup.zip',
+  'backup.tar',
+  'beez',
+  'bkp',
+  'build.xml',
+  'CMSHelp',
+  'CMSPages',
+  'CMSTemplates',
+  'db.php',
+  'dbconfig.zip',
+  'DesktopModule',
+  '_dump',
+  '.env',
+  'installer.php',
+  'item.php',
+  'mailer.php',
+  'magento',
+  'old/',
+  'preview.php',
+  '_profiler',
+  'sitecore',
+  'sitecorex',
+  'sitedata',
+  'site-tools',
+  'Site-Tools',
+  '.sql',
+  '.well-known',
+  'xleet',
+  'xmlrpc.php',
+  'var/',
+  'vendor/',
+  'wwwback',
+  'wwwdata',
+  'wwwroot',
 
-    // Weird old ASPX requests.
-    'AspxAutoDetectCookieSupport',
-    '(X(1)S',
+  // Weird old ASPX requests.
+  'AspxAutoDetectCookieSupport',
+  '(X(1)S',
 
-    // WP Paths.
-    'wordpress',
-    'wp-admin',
-    'wp-back',
-    'wp-config',
-    'wp-cli',
-    'wp-content',
-    'wp-include',
-    'wp-json',
-    'wp-login',
-    'wp-old',
-    'wp-plain',
-  ];
-  foreach($banned_urls as $url) {
-    if (stripos($_SERVER['REQUEST_URI'], $url) !== false) {
-      header('HTTP/1.0 403');
-      exit();
-    }
+  // WP Paths.
+  'wordpress',
+  'wp-admin',
+  'wp-back',
+  'wp-config',
+  'wp-cli',
+  'wp-content',
+  'wp-include',
+  'wp-json',
+  'wp-login',
+  'wp-old',
+  'wp-plain',
+];
+
+foreach($banned_urls as $url) {
+  if (stripos($_SERVER['REQUEST_URI'], $url) !== FALSE) {
+    header('HTTP/1.0 403');
+    exit();
   }
 }
